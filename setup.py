7eb407dfc6d7627c53ab4a086f7f168ca436957f
James Root <jamesroot@gmail.com>
2014-03-04 10:54:22 -0500

    Added webob to the dependencies

--- setup.py
+++ setup.py
@@ -15,9 +15,11 @@ setup(
     author_email='jdroot@sei.cmu.edu',
     url='',
     install_requires=[
+        "webob==1.0.8",
         "Pylons>=0.9.7",
         "Routes==1.11",
         "mako>=0.5",
+        "pymongo>=2.6.3"
         ],
     packages=find_packages(exclude=['ez_setup']),
     entry_points="""
