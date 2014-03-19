d0b638daee7f88b4f0aed695e2ef923fd305c5de
James Root <jamesroot@gmail.com>
2014-03-19 13:40:25 -0400

    Re-structured client folder

---
+++ client/client-lib/src/main/java/edu/cmu/sei/ams/cloudlet/Cloudlet.java
@@ -0,0 +1,11 @@
+package edu.cmu.sei.ams.cloudlet;
+
+/**
+ * User: jdroot
+ * Date: 3/19/14
+ * Time: 1:28 PM
+ */
+public class Cloudlet
+{
+
+}
---
+++ client/client-lib/src/main/java/edu/cmu/sei/ams/cloudlet/CloudletFinder.java
@@ -0,0 +1,11 @@
+package edu.cmu.sei.ams.cloudlet;
+
+/**
+ * User: jdroot
+ * Date: 3/19/14
+ * Time: 1:35 PM
+ */
+public class CloudletFinder
+{
+
+}
