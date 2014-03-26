0e640e26f112eff41577857d45abc8eb9bd1911b
James Root <jamesroot@gmail.com>
2014-03-26 16:45:03 -0400

    Start/Stopping of services working. Need to get Start App working

--- client/client-android/src/edu/cmu/sei/cloudlet/client/caching/ui/ListServicesActivity.java
+++ client/client-android/src/edu/cmu/sei/cloudlet/client/caching/ui/ListServicesActivity.java
@@ -1,22 +1,19 @@
 package edu.cmu.sei.cloudlet.client.caching.ui;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 import edu.cmu.sei.ams.cloudlet.Service;
+import edu.cmu.sei.ams.cloudlet.ServiceVM;
 import edu.cmu.sei.cloudlet.client.CurrentCloudlet;
 import edu.cmu.sei.cloudlet.client.R;
 import edu.cmu.sei.cloudlet.client.ui.ConnectionInfoFragment;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
 
@@ -35,6 +32,8 @@ public class ListServicesActivity extends Activity
     private ServiceRowAdapter adapter;
     private List<Service> serviceObjects;
 
+    private ProgressDialog mProgressDialog = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -48,7 +47,15 @@ public class ListServicesActivity extends Activity
         serviceObjects = new ArrayList<Service>();
         adapter = new ServiceRowAdapter(this, R.layout.cloudlet_app_item, serviceObjects);
         listView.setAdapter(adapter);
-        log.info("SERVICES ACTIVITY OPENED UP BABY");
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+//        {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+//            {
+//                log.info("Starting service");
+//                new StartServiceAsync().execute(serviceObjects.get(i));
+//            }
+//        });
 
         new GetServicesListAsync().execute();
     }
@@ -70,7 +77,7 @@ public class ListServicesActivity extends Activity
                 v = inflator.inflate(R.layout.cloudlet_service_item, null);
             }
 
-            Service service = getItem(position);
+            final Service service = getItem(position);
 
             TextView idTextView = (TextView)v.findViewById(R.id.idTextView);
             TextView descriptionTextView = (TextView)v.findViewById(R.id.descriptionTextView);
@@ -78,7 +85,138 @@ public class ListServicesActivity extends Activity
             idTextView.setText(service.getServiceId());
             descriptionTextView.setText(service.getServiceId());
 
+            final Button startStopServiceBtn = (Button)v.findViewById(R.id.startStopServiceButton);
+            final Button startAppBtn = (Button)v.findViewById(R.id.startAppButton);
+
+            final boolean running = service.getServiceVM() != null;
+
+            if (running)
+                startStopServiceBtn.setText("Stop Service");
+            else
+                startStopServiceBtn.setText("Start Service");
+
+            startStopServiceBtn.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+                    new StartStopServiceAsync(service, startStopServiceBtn, startAppBtn).execute();
+                }
+            });
+
             return v;
+        }
+    }
+
+    private class StartStopServiceAsync extends AsyncTask<Void, Void, Boolean>
+    {
+        private Service service;
+        private Button startStopButton;
+        private Button appButton;
+
+        private StartStopServiceAsync(Service service, Button startStopButton, Button appButton)
+        {
+            this.service = service;
+            this.startStopButton = startStopButton;
+            this.appButton = appButton;
+        }
+
+        @Override
+        protected void onPreExecute()
+        {
+            mProgressDialog = new ProgressDialog(ListServicesActivity.this);
+            mProgressDialog.setTitle("Service VM");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            if (service.getServiceVM() == null)
+                mProgressDialog.setMessage("Starting Service VM");
+            else
+                mProgressDialog.setMessage("Stopping Service VM");
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... voids)
+        {
+            if (service.getServiceVM() == null)
+            {
+                service.startService();
+                return true;
+            }
+            else
+            {
+                service.stopService();
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean startedOrStopped)
+        {
+            if (mProgressDialog != null)
+                mProgressDialog.dismiss();
+            if (startedOrStopped.booleanValue())
+            {
+                if (service.getServiceVM() == null) //Failed to start
+                {
+                    Toast.makeText(ListServicesActivity.this, "Error while starting service!", Toast.LENGTH_SHORT).show();
+                    startStopButton.setText("Start Service");
+                    appButton.setEnabled(false);
+                }
+                else
+                {
+                    Toast.makeText(ListServicesActivity.this, "Service started!", Toast.LENGTH_SHORT).show();
+                    startStopButton.setText("Stop Service");
+                    appButton.setEnabled(true);
+                }
+            }
+            else
+            {
+                if (service.getServiceVM() == null) //Stopped
+                {
+                    Toast.makeText(ListServicesActivity.this, "Service stopped!", Toast.LENGTH_SHORT).show();
+                    startStopButton.setText("Start Service");
+                    appButton.setEnabled(false);
+                }
+                else
+                {
+                    Toast.makeText(ListServicesActivity.this, "Error while stopping service!", Toast.LENGTH_SHORT).show();
+                    startStopButton.setText("Stop Service");
+                    appButton.setEnabled(true);
+                }
+            }
+        }
+    }
+
+    private class StartServiceAsync extends AsyncTask<Service, Void, ServiceVM>
+    {
+        @Override
+        protected void onPreExecute()
+        {
+            mProgressDialog = new ProgressDialog(ListServicesActivity.this);
+            mProgressDialog.setTitle("Starting VM");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mProgressDialog.setMessage("Starting service vm");
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected ServiceVM doInBackground(Service... services)
+        {
+            Service s = services[0];
+            return s.startService();
+        }
+
+        @Override
+        protected void onPostExecute(ServiceVM vm)
+        {
+            if (mProgressDialog != null)
+                mProgressDialog.dismiss();
+            if (vm != null)
+                Toast.makeText(ListServicesActivity.this, "Service started!", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(ListServicesActivity.this, "Error while starting service!", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -86,6 +224,17 @@ public class ListServicesActivity extends Activity
     {
 
         private Exception e;
+
+        @Override
+        protected void onPreExecute()
+        {
+            mProgressDialog = new ProgressDialog(ListServicesActivity.this);
+            mProgressDialog.setTitle("Querying Services");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mProgressDialog.setMessage("Getting services from " + CurrentCloudlet.cloudlet.getName());
+            mProgressDialog.show();
+        }
 
         @Override
         protected List<Service> doInBackground(Void... voids)
@@ -106,6 +255,10 @@ public class ListServicesActivity extends Activity
         @Override
         protected void onPostExecute(List<Service> services)
         {
+            if (mProgressDialog != null)
+                mProgressDialog.dismiss();
+
+
             if (services == null)
             {
                 Toast.makeText(ListServicesActivity.this, "Error getting app list: " + e.getMessage(), Toast.LENGTH_SHORT).show();
