da3230fa02d403d94a191dc795ce46c9e2ca7c81
secheverria <sebastian.echeverria@gmail.com>
2014-05-02 13:17:22 -0400

    API:  * Deleted some code that had already been moved, and was readded in some merge.
    Diagram:
     * Updated static diagram
    
    Pycloud Lib:
     * Removed dependency from SVMManager to InstanceManager (only functions that needed this were moved back into SVMTool)

--- server/pycloud/api/controllers/servicevm.py
+++ server/pycloud/api/controllers/servicevm.py
@@ -46,60 +46,6 @@ class ServiceVMController(BaseController):
         # Cleanup running vms.
         if(self.runningVMManager != None):
             self.runningVMManager.cleanup()
-            
-    ################################################################################################################
-    # Get a list of the services in the VM. In realiy, for now at least, it actually get a list of services that
-    # have associated ServiceVMs in the cache.
-    ################################################################################################################
-    @asjson
-    def GET_listServices(self):
-        print '\n*************************************************************************************************'
-        timelog.TimeLog.reset()
-        timelog.TimeLog.stamp("Request received: get list of Services.")
-        
-        # Get the list of stored service vms in the cache.
-        serviceVmCache = svmrepository.ServiceVMRepository(g.cloudlet)
-        vmList = serviceVmCache.getStoredServiceVMList()
-
-        # Create an item list with the info to display.
-        servicesList = []
-    	for storedVMId in vmList:
-            storedVM = vmList[storedVMId]
-            serviceInfo = {'_id': storedVM.metadata.serviceId,
-                           'description': storedVM.name}
-            servicesList.append(serviceInfo)        
-        
-        # Create a JSON response to indicate the result.
-        jsonDataStructure = {"services": servicesList}
-        
-        # Send the response.
-        timelog.TimeLog.stamp("Sending response back to " + request.environ['REMOTE_ADDR'])
-        return jsonDataStructure
-            
-    ################################################################################################################
-    # Called to check if a specific service is already provided by a cached Service VM on this server.
-    ################################################################################################################
-    def GET_find(self):
-        # Look for the VM in the repository.
-        serviceId = request.GET['serviceId']
-        vmFound = False
-        print '\n*************************************************************************************************'
-        timelog.TimeLog.reset()
-        timelog.TimeLog.stamp("Request received: find cached Service VM.")
-        serviceVmRepo = svmrepository.ServiceVMRepository(g.cloudlet)
-        vmEntry = serviceVmRepo.findServiceVM(serviceId)
-        if(vmEntry != None):
-            vmFound = True
-        
-        # Create a JSON response to indicate the result.
-        jsonDataStructure = { "VM_EXISTS" : str(vmFound)
-                            }
-        jsonDataString = json.dumps(jsonDataStructure)
-        
-        # Send the response.
-        timelog.TimeLog.stamp("Sending response back to " + request.environ['REMOTE_ADDR'])
-        responseText = jsonDataString
-        return responseText          
         
     ################################################################################################################
     # Called to start a Service VM.
