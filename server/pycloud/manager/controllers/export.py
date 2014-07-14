7493ff61dfb8a1f7449720b109441c4734faec61
James Root <jamesroot@gmail.com>
2014-07-14 14:42:49 -0400

    slowly upating

--- server/pycloud/manager/controllers/export.py
+++ server/pycloud/manager/controllers/export.py
@@ -1,13 +1,22 @@
 __author__ = 'jdroot'
 
 from pycloud.pycloud.pylons.lib.base import BaseController
+from pycloud.pycloud.model import Service
 from pylons import request, g
+import os
 
 
 class ExportController(BaseController):
 
     def GET_export_svm(self, sid):
+
+        service = Service.by_id(sid)
+
         path = request.params.get("export_path")
         if not path:
-            path = g.cloudlet.export_path + sid + ".csvm"
-        return "Sid: " + sid + "\n" + path
+            path = os.path.join(g.cloudlet.export_path, service['_id'] + ".csvm")
+
+        print "Disk Image: ", service.vm_image.disk_image
+        print "State Image: ", service.vm_image.state_image
+
+        return "Sid: " + sid + "<br>" + path
