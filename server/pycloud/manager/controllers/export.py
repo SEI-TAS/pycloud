ea1d416bfac9d2195f82df92a1a3abe3c248c673
James Root <jamesroot@gmail.com>
2014-07-15 15:47:59 -0400

    Trying to use Tar files now

--- server/pycloud/manager/controllers/export.py
+++ server/pycloud/manager/controllers/export.py
@@ -4,8 +4,7 @@ from pycloud.pycloud.pylons.lib.base import BaseController
 from pycloud.pycloud.model import Service
 from pylons import request, g
 import os
-import zipfile
-
+import tarfile
 
 class ExportController(BaseController):
 
@@ -23,9 +22,11 @@ class ExportController(BaseController):
         print "Disk Image: ", service.vm_image.disk_image
         print "State Image: ", service.vm_image.state_image
 
-        zipf = zipfile.ZipFile(path, "w", allowZip64=True)  # Needed for files larger than 2gb
-        zipf.write(service.vm_image.disk_image)
-        zipf.write(service.vm_image.state_image)
+
+
+        zipf = tarfile.open(path, "w:gz")
+        zipf.add(service.vm_image.disk_image)
+        zipf.add(service.vm_image.state_image)
         zipf.close()
 
         return "Ok"
