e1fbd182d1eefa22ebb8d2ba41bb4a439cab39a6
James Root <jamesroot@gmail.com>
2014-07-29 11:47:00 -0400

    Testing export

--- server/pycloud/manager/controllers/export.py
+++ server/pycloud/manager/controllers/export.py
@@ -4,14 +4,13 @@ from pycloud.pycloud.pylons.lib.base import BaseController
 from pycloud.pycloud.pylons.lib.util import asjson
 from pycloud.pycloud.model import Service
 from pylons import request, g
-import os
+import os, tarfile, StringIO
 
 
 class ExportController(BaseController):
 
     @asjson
     def GET_export_svm(self, sid):
-        # sid = request.params.get("sid")
         service = Service.by_id(sid)
 
         if not service:
@@ -24,11 +23,22 @@ class ExportController(BaseController):
         print "Disk Image: ", service.vm_image.disk_image
         print "State Image: ", service.vm_image.state_image
 
+        tar = tarfile.open(path, "w:gz")
+        tar.add(service.vm_image.disk_image)
+        tar.add(service.vm_image.state_image)
+        add_string_to_tar(data=service.export(), filename=service.service_id + ".json", tar=tar)
+        tar.close()
+
+        return path
 
 
-        # tarf = tarfile.open(path, "w:gz")
-        # tarf.add(service.vm_image.disk_image)
-        # tarf.add(service.vm_image.state_image)
-        # tarf.close()
+def add_string_to_tar(data=None, filename=None, tar=None):
+    if not data or not filename or not tar:
+        return
+    sio = StringIO.StringIO()
+    sio.write(data)
+    sio.seek(0)
 
-        return service.export()
+    tarinfo = tarfile.TarInfo(name=filename)
+    tarinfo.size = len(sio.buf)
+    tar.addfile(tarinfo=tarinfo, fileobj=sio)
