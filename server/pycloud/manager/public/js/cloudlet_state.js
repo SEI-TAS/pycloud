0d3e802067ed54df0e4e7fcde23f8a3af630577b
sebastian_echeverria <secheverria@miuandes.cl>
2014-09-09 17:15:42 -0300

    CM: Added missing js.

---
+++ server/pycloud/manager/public/js/cloudlet_state.js
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Functions used to handle cloudlet state.
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////////////
+// Function to update status info.
+/////////////////////////////////////////////////////////////////////////////////////
+function reloadState()
+{
+    var statusUrl = $("#state_url").val();
+    ajaxGet(statusUrl, waitDialogText=null, function(response) {
+        // Check if we got a correctly formed JSON.
+        if(response.hasOwnProperty('cpu_info'))
+        {
+            $('#cpu_load').text(response.cpu_info.usage);
+            $('#cpu_cores').text(response.cpu_info.max_cores);
+
+            var free_memory = parseFloat(response.memory_info.free_memory) / (1024.0 * 1024.0 * 1024.0);
+            var max_memory = parseFloat(response.memory_info.max_memory) / (1024.0 * 1024.0 * 1024.0);
+            var mem_load = parseFloat(free_memory)/parseFloat(max_memory) * 100.0;
+            $('#mem_load').text(mem_load.toFixed(2));
+            $('#mem_free').text(free_memory.toFixed(2));
+            $('#mem_max').text(max_memory.toFixed(2));
+        }
+        else
+        {
+            console.log('Error getting cloudlet status. Got: ' + JSON.stringify(response));
+        }
+
+        // To be called again after the next interval.
+        var reloadInterval = 2000; // In milliseconds.
+        setTimeout(function() {reloadState(statusUrl);}, reloadInterval);
+    });
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// Called when the document is loaded.
+/////////////////////////////////////////////////////////////////////////////////////////////////
+$(document).ready( function () {
+    // Start the load checker, which will check for changes continuously.
+    console.log('Setting up reload state.');
+    reloadState();
+});    
