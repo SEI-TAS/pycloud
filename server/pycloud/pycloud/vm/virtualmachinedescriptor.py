2b79d7a67d034d402afde6f4a8fa28242f279433
James Root <jamesroot@gmail.com>
2014-11-25 15:09:33 -0500

    Future proofed an if statement

--- server/pycloud/pycloud/vm/virtualmachinedescriptor.py
+++ server/pycloud/pycloud/vm/virtualmachinedescriptor.py
@@ -67,7 +67,7 @@ class VirtualMachineDescriptor(object):
         # TODO: We need to evaluate if this is the correct approach
         # TODO: We need to determine if there are other interface types besides 'user'
         user = devices.find("interface[@type='user']")
-        if user:
+        if user is not None:
             devices.remove(user)
 
         mac = self.randomMAC()
