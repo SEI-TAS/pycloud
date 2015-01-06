4c8b0022d69a8c0d7e5030e7c68f8affb471a6cd
sebastian_echeverria <secheverria@miuandes.cl>
2015-01-06 11:35:53 -0300

    CM: more temp changes to track down permissions issues.

--- server/pycloud/pycloud/model/vmimage.py
+++ server/pycloud/pycloud/model/vmimage.py
@@ -96,8 +96,8 @@ class VMImage(DictObject):
             fileutils.FileUtils.recreateFolder(destination_folder)
             
             # Move the files.
-            shutil.move(self.disk_image, destination_folder)
-            shutil.move(self.state_image, destination_folder)
+            shutil.copyfile(self.disk_image, destination_folder)
+            shutil.copyfile(self.state_image, destination_folder)
             
             # Update our paths to reflect the move.
             source_dir = os.path.dirname(self.disk_image)
