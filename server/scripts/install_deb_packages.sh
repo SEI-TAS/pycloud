f392bcdc37b9a82d261f7e0c6c8c5b8d728c9965
sebastian_echeverria <secheverria@miuandes.cl>
2015-03-04 17:38:51 -0300

    Updated scripts to set up local dev environment with nmap too.

--- server/scripts/install_deb_packages.sh
+++ server/scripts/install_deb_packages.sh
@@ -4,7 +4,7 @@
 apt-get update
 
 # Dependencies for the Cloudlet Server.
-apt-get install qemu-kvm libvirt-bin gvncviewer python2.7 python-dev python-pip python-libvirt mongodb
+apt-get install qemu-kvm libvirt-bin nmap python2.7 python-dev python-pip python-libvirt mongodb
 
 # Dependencies for the Discovery Server.
 apt-get install libnss-mdns avahi-daemon
