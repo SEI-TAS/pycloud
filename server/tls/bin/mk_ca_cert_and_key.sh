79d090896ad2cb6f7a5e21ed4a4e543c5c8eceea
secheverria <sebastian.echeverria@gmail.com>
2015-03-13 16:29:54 -0300

    TLS: more script bug fixes.

--- server/tls/bin/mk_ca_cert_and_key.sh
+++ server/tls/bin/mk_ca_cert_and_key.sh
@@ -24,12 +24,12 @@ test -z STATE && error "You must supply a state"
 test -z LOCALITY && error "You must supply a locality"
 
 # Store org info.
-rm -f ORG_INFO_FILE
-touch ORG_INFO_FILE
-echo "ORG=$ORG" >> ORG_INFO_FILE
-echo "COUNTRY=$COUNTRY" >> ORG_INFO_FILE
-echo "STATE=$STATE" >> ORG_INFO_FILE
-echo "LOCALITY=$LOCALITY" >> ORG_INFO_FILE
+rm -f $ORG_INFO_FILE
+touch $ORG_INFO_FILE
+echo "ORG=$ORG" >> $ORG_INFO_FILE
+echo "COUNTRY=$COUNTRY" >> $ORG_INFO_FILE
+echo "STATE=$STATE" >> $ORG_INFO_FILE
+echo "LOCALITY=$LOCALITY" >> $ORG_INFO_FILE
 
 NAME=certificate_authority
 
