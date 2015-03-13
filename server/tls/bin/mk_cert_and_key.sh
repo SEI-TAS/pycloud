11a2232788b983b69d9e16215305351e3d9e877f
secheverria <sebastian.echeverria@gmail.com>
2015-03-13 17:48:33 -0300

    TLS: clarification and tests.

--- server/tls/bin/mk_cert_and_key.sh
+++ server/tls/bin/mk_cert_and_key.sh
@@ -24,14 +24,16 @@ test -e $CA_CERT || error "CA does not exist"
 test -e $CA_PRIV_KEY || error "CA key does not exist"
 test -e $ORG_INFO_FILE || error "Information file about organization does not exist"
 
-# Load organizational info: ORG, COUNTRY, STATE, LOCALITY
-srcdir=$(readlink -m $(dirname $0))
-source $ORG_INFO_FILE
-
 NAME=$HOST.$TYPE
 
 echo "Generating private key"
 $srcdir/_mk_key.sh $NAME $OUTPUT_FOLDER
+
+# Load organizational info: ORG, COUNTRY, STATE, LOCALITY
+source $ORG_INFO_FILE
+
+echo $ORG
+echo "check"
 
 TEMP_INFO_FILE=$(mktemp)
 $srcdir/_mk_$TYPE_info.sh $HOST $ORG $COUNTRY $STATE $LOCALITY > $TEMP_INFO_FILE
