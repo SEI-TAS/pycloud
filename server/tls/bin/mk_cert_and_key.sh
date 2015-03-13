48138130d9e84f9c52c8a07706e83e8870e75766
secheverria <sebastian.echeverria@gmail.com>
2015-03-13 17:56:32 -0300

    TLS: tests

--- server/tls/bin/mk_cert_and_key.sh
+++ server/tls/bin/mk_cert_and_key.sh
@@ -14,17 +14,19 @@ srcdir=$(readlink -m $(dirname $0))
 source $srcdir/common
 
 TYPE=$1
-echo $TYPE
-echo $srcdir/_mk_$TYPE_info.sh
 HOST=$2
+
+echo $TYPE
+echo $srcdir/_mk$TYPE_info.sh
+
 
 CA_CERT=$OUTPUT_FOLDER/certificate_authority.certificate.pem
 CA_PRIV_KEY=$CA_KEY_FOLDER/certificate_authority.key.pem
 
 test -z $HOST && error "You must supply a host"
-test -e $CA_CERT || error "CA does not exist"
-test -e $CA_PRIV_KEY || error "CA key does not exist"
-test -e $ORG_INFO_FILE || error "Information file about organization does not exist"
+test -e $CA_CERT && error "CA does not exist"
+test -e $CA_PRIV_KEY && error "CA key does not exist"
+test -e $ORG_INFO_FILE && error "Information file about organization does not exist"
 
 NAME=$HOST.$TYPE
 
