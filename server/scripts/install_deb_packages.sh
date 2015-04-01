7383c102bbf8cf593f4e5373766db858b91eb05b
secheverria <sebastian.echeverria@gmail.com>
2015-03-31 22:52:39 -0300

    Scripts: updating scripts so that they can actually work to set up a development environment, updated.

--- server/scripts/install_deb_packages.sh
+++ server/scripts/install_deb_packages.sh
@@ -4,7 +4,7 @@
 apt-get update
 
 # Dependencies for the Cloudlet Server.
-apt-get install qemu-kvm libvirt-bin nmap python2.7 python-dev python-pip python-libvirt mongodb
+apt-get install qemu-kvm libvirt-bin nmap python2.7 python-dev python-libvirt mongodb nmap
 
 # Dependencies for the Discovery Server.
 apt-get install libnss-mdns avahi-daemon
--- server/scripts/venv_setup.sh
+++ server/scripts/venv_setup.sh
@@ -5,8 +5,7 @@ apt-get install python-pip
 pip install virtualenv
 
 # Create a virtual env to work on.
-virtualenv --no-site-packages ./env
+virtualenv --no-site-packages ../env
 
 # Copy libvirt python packages to the env (there is no way to install them directly through pip).
-apt-get install python-libvirt
-cp /usr/lib/python2.7/dist-packages/*libvirt* ./env/lib/python2.7/site-packages/
+cp /usr/lib/python2.7/dist-packages/*libvirt* ../env/lib/python2.7/site-packages/
