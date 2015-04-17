d5f408c9525c48d11bb92f370500ab235d947de0
secheverria <sebastian.echeverria@gmail.com>
2015-04-17 13:22:36 -0300

    Adding license to all licensable files. Bash scripts will not have one, at least for now, since it breaks the shebang line.

--- pycloud/manager/public/js/svm_state.js
+++ pycloud/manager/public/js/svm_state.js
@@ -1,3 +1,32 @@
+/*
+KVM-based Discoverable Cloudlet (KD-Cloudlet) 
+Copyright (c) 2015 Carnegie Mellon University.
+All Rights Reserved.
+
+THIS SOFTWARE IS PROVIDED "AS IS," WITH NO WARRANTIES WHATSOEVER. CARNEGIE MELLON UNIVERSITY EXPRESSLY DISCLAIMS TO THE FULLEST EXTENT PERMITTEDBY LAW ALL EXPRESS, IMPLIED, AND STATUTORY WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT OF PROPRIETARY RIGHTS.
+
+Released under a modified BSD license, please see license.txt for full terms.
+DM-0002138
+
+KD-Cloudlet includes and/or makes use of the following Third-Party Software subject to their own licenses:
+MiniMongo
+Copyright (c) 2010-2014, Steve Lacy 
+All rights reserved. Released under BSD license.
+https://github.com/MiniMongo/minimongo/blob/master/LICENSE
+
+Bootstrap
+Copyright (c) 2011-2015 Twitter, Inc.
+Released under the MIT License
+https://github.com/twbs/bootstrap/blob/master/LICENSE
+
+jQuery JavaScript Library v1.11.0
+http://jquery.com/
+Includes Sizzle.js
+http://sizzlejs.com/
+Copyright 2005, 2014 jQuery Foundation, Inc. and other contributors
+Released under the MIT license
+http://jquery.org/license
+*/
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // Functions used to handle cloudlet state.
 /////////////////////////////////////////////////////////////////////////////////////////////////
