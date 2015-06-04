382b00064435239f0f7061c424f7846c1bd98aab
secheverria <sebastian.echeverria@gmail.com>
2015-06-04 10:14:40 -0300

    ADBDevice: created basic ADB device following the general interface. It is currently able to list devices, connect and disconnect.

--- pycloud/pycloud/ska/adb_ska_device.py
+++ pycloud/pycloud/ska/adb_ska_device.py
@@ -36,18 +36,105 @@ import cStringIO
 
 from ska_device_interface import ISKADevice
 
-for device in AdbCommands.Devices():
-    print device
-    print device.serial_number
-    print device.port_path
-
-    print 'Connecting to USB device'
-    device.Open()
-
+######################################################################################################################
+# Attempts to connect to an ADB daemon on a given USB device. Returns a proxy to that daemon if succesful, or None if
+# unsuccessful
+######################################################################################################################
+def connect_to_adb_daemon(device):
+    # Load host keypair to authenticate with ADB deamon.
     keySigner = M2CryptoSigner('adbkey')
 
-    print 'Connecting to ADB daemon'
-    adb = AdbCommands.Connect(device, banner="cloudlet", rsa_keys=[keySigner])
+    try:
+        print 'Connecting to USB device'
+        device.Open()
+
+        print 'Connecting to ADB daemon'
+        adbDaemon = AdbCommands.Connect(device, banner="cloudlet", rsa_keys=[keySigner])
+        return adbDaemon
+    except Exception, e:
+        print e
+        return None
+
+######################################################################################################################
+#
+######################################################################################################################
+def disconnect_from_adb_daemon(adbDaemon):
+    adbDaemon.Close()
+
+######################################################################################################################
+# Implementation of a SKA device through ADB.
+######################################################################################################################
+class ADBSKADevice(ISKADevice):
+
+    serial_number = None
+    adb_daemon = None
+
+    ####################################################################################################################
+    # Creates a device using the provided device info.
+    ####################################################################################################################
+    def __init__(self, device):
+        self.serial_number = device.serial_number
+
+    ####################################################################################################################
+    # Returns a list of ADBSKADevices. (Actually, it contains all USB devices connected to the computer).
+    ####################################################################################################################
+    @staticmethod
+    def list_devices():
+        usb_devices = []
+        for device in AdbCommands.Devices():
+            usb_devices.append(ADBSKADevice(device))
+
+        return usb_devices
+
+    ####################################################################################################################
+    # Connects to the adb daemon on the given device (by serial number).
+    ####################################################################################################################
+    def connect(self):
+        if self.serial_number is None:
+            print 'No serial number configured for device.'
+            return False
+
+        for device in AdbCommands.Devices():
+            if device.serial_number == self.serial_number:
+                self.adb_daemon = connect_to_adb_daemon(device)
+                if self.adb_daemon is not None:
+                    return True
+                else:
+                    print 'Unable to connect to ADB daemon on device ' + self.serial_number + '.'
+                    return False
+
+        print 'Given device ' + self.serial_number + ' not found.'
+        return False
+
+    ####################################################################################################################
+    # Disconnects from the ADB daemon, and the USB device.
+    ####################################################################################################################
+    def disconnect(self):
+        if self.adb_daemon:
+            disconnect_from_adb_daemon(self.adb_daemon)
+
+    def get_id(self):
+        raise NotImplementedError()
+
+    def send_master_public_key(self, file_path):
+        raise NotImplementedError()
+
+    def send_device_private_key(self, file_path):
+        raise NotImplementedError()
+
+    def send_server_certificate(self, file_path):
+        raise NotImplementedError()
+
+
+devices = ADBSKADevice.list_devices()
+if len(devices) > 0:
+    device = devices[0]
+    print device
+    print device.serial_number
+
+    adbDevice = ADBSKADevice(device)
+    adbDevice.connect()
+    adb = adbDevice.adb_daemon
 
     print 'Sending data'
     adb.Push(cStringIO.StringIO('this is a test'), '/sdcard/test.txt')
@@ -59,5 +146,4 @@ for device in AdbCommands.Devices():
     print 'Starting activity'
     adb.Shell('am start -n edu.cmu.sei.cloudlet.client/.ui.NewProcessSelectionActivity', timeout_ms=10000)
 
-    device.Close()
-
+    adbDevice.disconnect()
