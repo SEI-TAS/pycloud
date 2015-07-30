4775ef20dce141eb45a1ff1a582c7fc69362aa04
secheverria <sebastian.echeverria@gmail.com>
2015-07-30 12:35:47 -0300

    PKI: implemented and tested function to generate self-signed certificate.

--- pycloud/pycloud/utils/pki.py
+++ pycloud/pycloud/utils/pki.py
@@ -27,35 +27,59 @@
 # http://jquery.org/license
 __author__ = 'Sebastian'
 
-from OpenSSL import crypto, SSL
 from socket import gethostname
+from M2Crypto import X509, RSA, ASN1, EVP
+import time
+
+import rsa
+
+# Standard times to be used for any certificate: valid from now to 10 more years.
+t = long(time.time()) + time.timezone
+before = ASN1.ASN1_UTCTIME()
+before.set_time(t)
+after = ASN1.ASN1_UTCTIME()
+after.set_time(t + 60 * 60 * 24 * 365 * 10) # 10 years
 
 #############################################################################################
 #
 #############################################################################################
 def create_self_signed_cert(cert_file_path, private_key_file_path):
+    cert = X509.X509()
+    cert.set_version(2)
 
-    # create a key pair
-    k = crypto.PKey()
-    k.generate_key(crypto.TYPE_RSA, 1024)
-
-    # create a self-signed cert
-    cert = crypto.X509()
+    # Create subject info.
     cert.get_subject().C = "USA"
     cert.get_subject().ST = "Cloudlet"
     cert.get_subject().L = "Cloudlet"
     cert.get_subject().O = "Pycloud"
     cert.get_subject().OU = "Pycloud"
     cert.get_subject().CN = gethostname()
+
+    # Create secondary params.
     cert.set_serial_number(1000)
-    cert.gmtime_adj_notBefore(0)
-    cert.gmtime_adj_notAfter(10*365*24*60*60)
+    cert.set_not_before(before)
+    cert.set_not_after(after)
+
+    # It is self-signed.
     cert.set_issuer(cert.get_subject())
-    cert.set_pubkey(k)
-    cert.sign(k, 'sha1')
 
-    open(cert_file_path, "wt").write(
-        crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
-    open(private_key_file_path, "wt").write(
-        crypto.dump_privatekey(crypto.FILETYPE_PEM, k))
+    # Generate an RSA key pair, store it in files, and load it.
+    rsa.create_key_pair(private_key_file_path, private_key_file_path + ".pub")
+    public_key = RSA.load_pub_key(private_key_file_path + ".pub")
+    private_key = RSA.load_key(private_key_file_path)
 
+    # Associate the public key we just generated.
+    request_public_key = EVP.PKey()
+    request_public_key.assign_rsa(public_key)
+    cert.set_pubkey(request_public_key)
+
+    # Sign the certificate.
+    request_private_key = EVP.PKey()
+    request_private_key.assign_rsa(private_key)
+    cert.sign(request_private_key, 'sha256')
+
+    # Store the certificate.
+    cert.save(cert_file_path)
+
+# Test
+#create_self_signed_cert('./cert.pem', './key.rsa')
