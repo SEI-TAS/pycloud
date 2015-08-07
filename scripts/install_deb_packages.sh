862de23c8e9fc01c29aabba77d77d113a718e761
secheverria <sebastian.echeverria@gmail.com>
2015-08-07 13:59:25 -0300

    Fixed dependency and links for installer.

--- scripts/install_deb_packages.sh
+++ scripts/install_deb_packages.sh
@@ -5,7 +5,7 @@ apt-get update
 
 # Dependencies for the Cloudlet Server.
 apt-get -y install qemu-kvm libvirt-bin nmap python2.7 python-dev python-libvirt mongodb
-apt-get -y install libusb-1.0.0-dev libbluetooth-dev build-essential swig libssl-dev libffi-dev
+apt-get -y install libusb-1.0-0-dev libbluetooth-dev build-essential swig libssl-dev libffi-dev
 
 # Dependencies for the Discovery Server.
 apt-get install libnss-mdns avahi-daemon
