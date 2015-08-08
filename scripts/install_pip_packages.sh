1c7c98324d3070cd4253f4c896e33df5c4cdee11
secheverria <sebastian.echeverria@gmail.com>
2015-08-08 00:28:04 -0300

    Moved python-adb to fork in pypi, so it can be installed by pip transparently.

--- scripts/install_pip_packages.sh
+++ scripts/install_pip_packages.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 source ../env/bin/activate
 pip install -r ../requirements.txt
-
-# python-adb is not in the PyPi package repo, it has to be installed separately.
-bash python-adb_setup.sh
