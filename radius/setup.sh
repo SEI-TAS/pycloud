e1e364dda91f48e2c5f605bba86ccf52326e14e2
secheverria <sebastian.echeverria@gmail.com>
2015-09-25 12:08:18 -0300

    Added script to automatically set up cron job to reset FreeRadius when configs change.

--- radius/setup.sh
+++ radius/setup.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
+# NOTE: This script should be called with root permissions.
 
+SCRIPTS_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# Copy fixed Upstart job that will allow reload to work.
+cp freeradius.conf /etc/init
+
+# Create cron job that will call our scripts to constantly check for changes in the config and to reload FreeRADIUS when
+# this happens
+cp freerad /etc/cron.d
+
+# Update the cron job so that it has the appropriate paths.
+sed -i -e "s:pycloud_scripts_path:$SCRIPTS_PATH:g" /etc/cron.d/freerad
+
+echo "Finished setting up cron job"
