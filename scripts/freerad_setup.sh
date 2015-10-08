54040d1d6df8d30970843e5468585ee4be3e782f
secheverria <sebastian.echeverria@gmail.com>
2015-10-08 12:10:48 -0300

    More script bug fixing.

--- scripts/freerad_setup.sh
+++ scripts/freerad_setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # Give group freeradius write permissions to the main FreeRADIUS folder so that pycloud can modify configurations.
+chown -R freerad:freerad /etc/freeradius/
 chmod -R g+rw /etc/freeradius/
