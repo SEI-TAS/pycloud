74ac39158e5e02c77d7190cae2e7d66c0b7f6020
secheverria <sebastian.echeverria@gmail.com>
2015-10-08 11:15:37 -0300

    Changing permissions setup from user change to group write permissions for FreeRADIUS. Not yet tested.

--- scripts/freerad_setup.sh
+++ scripts/freerad_setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-chown -R $USER /etc/freeradius
+# Give group freeradius write permissions to the main FreeRADIUS folder so that pycloud can modify configurations.
+chmod -R g+rw /etc/freeradius/
