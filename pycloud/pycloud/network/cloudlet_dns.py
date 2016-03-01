d3318f58b5b2c1e3903dc25b957210462e3bafb1
secheverria <sebastian.echeverria@gmail.com>
2016-03-01 00:56:20 -0300

    Scripts: fixes to DNS setup script after testing it. It works, but the resulting config files have not been fully tested yet.

--- pycloud/pycloud/network/cloudlet_dns.py
+++ pycloud/pycloud/network/cloudlet_dns.py
@@ -28,30 +28,13 @@
 
 import dynamic_dns
 import os
-import sys
+from pycloud.pycloud.network.tsig import load_tsig_key
 
 SVMS_ZONE_NAME = 'svm.cloudlet.local.'
 CLOUDLET_HOST_NAME = 'cloudlet'
 
 # Internal file path, relative to data folder.
 KEY_FILE_PATH = 'dns/Ksvm.cloudlet.local.private'
-
-#################################################################################################################
-# Loads the key value from a TSIG privat key file.
-#################################################################################################################
-def load_tsig_key(key_path):
-    key_value = None
-    with open(key_path, "r") as key_file:
-        # Find the key in the formatted key file, it is the text after a line starting with "Key: "
-        data = key_file.read()
-        lines = data.splitlines()
-        for line in lines:
-            parts = line.split(': ')
-            if len(parts) > 1 and parts[0] == 'Key':
-                key_value = parts[1]
-                break
-
-    return key_value
 
 #################################################################################################################
 # Object used to manage the cloudlet DNS server.
@@ -92,10 +75,3 @@ class CloudletDNS(object):
             return
 
         dynamic_dns.remove_dns_record(SVMS_ZONE_NAME, self.key, svm_fqdn)
-
-#################################################################################################################
-# Main behavior is to load a TSIG file and print its contents.
-#################################################################################################################
-if __name__ == '__main__':
-    key = load_tsig_key(sys.argv[1])
-    print key
--- scripts/dns_setup.sh
+++ scripts/dns_setup.sh
@@ -2,33 +2,40 @@
 
 # Needs to be called with sudo.
 
+echo 'Copying templates.'
+
 # Copy zone file template.
-cp ../dns/db.svm.cloudlet.local /var/lib/bind/
+sudo cp ../dns/db.svm.cloudlet.local /var/lib/bind/
 
 # Copy config file template.
-cp ../dns/named.conf.local /etc/bind/
+sudo cp ../dns/named.conf.local /etc/bind/
 
 # TODO: Get the cloudlet's public IP.
 CLOUDLET_PUBLIC_IP=192.168.0.112
 
 # Set IP in zone file.
-sed -i -e "s:CLOUDLET_PUBLIC_IP:$CLOUDLET_PUBLIC_IP:g" /var/lib/bind/db.svm.cloudlet.local
+echo 'Setting IP in zone file.'
+sudo sed -i -e "s:CLOUDLET_PUBLIC_IP:$CLOUDLET_PUBLIC_IP:g" /var/lib/bind/db.svm.cloudlet.local
 
 # Generate and load TSIG key.
-dnssec-keygen -a HMAC-MD5 -b 512 -n USER svm.cloudlet.local
+echo 'Generating TSIG key.'
+dnssec-keygen -a HMAC-MD5 -b 512 -n HOST -r /dev/urandom  svm.cloudlet.local
 mv Ksvm.cloudlet.local*.private Ksvm.cloudlet.local.private
-TSIG_PASSWORD=`python ../pycloud/pycloud/network/cloudlet_dns.py ./Ksvm.cloudlet.local.private`
+TSIG_PASSWORD=`python ../pycloud/pycloud/network/tsig.py ./Ksvm.cloudlet.local.private`
 
 # TODO: CopyTSIG key to data folder.
 mkdir ../data/dns
 mv ./Ksvm.cloudlet.local.private ../data/dns/
 
 # Update the config with the newly created TSIG key.
-sed -i -e "s:TSIG_PASSWORD:$TSIG_PASSWORD:g" /etc/bind/named.conf.local
+echo 'Setting TSIG key in config file.'
+sudo sed -i -e "s:TSIG_PASSWORD:$TSIG_PASSWORD:g" /etc/bind/named.conf.local
 
 # Disable dnsmasq in NetworkManager and restart NetworkManager.
-sed -i -e "s:dns=dnsmasq:#dns=dnsmasq:g" /etc/NetworkManager/NetworkManager.conf
-service network-manager restart
+echo 'Disabling dnsmasq.'
+sudo sed -i -e "s:dns=dnsmasq:#dns=dnsmasq:g" /etc/NetworkManager/NetworkManager.conf
+sudo service network-manager restart
 
 # Restart bind.
-service bind9 restart
+echo 'Restarting bind.'
+sudo service bind9 restart
