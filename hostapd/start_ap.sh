b657bbfe7ac59b01b92c709cc6f13825bbba1c32
secheverria <sebastian.echeverria@gmail.com>
2016-03-14 20:28:07 -0300

    AP: created scripts that start and stop an AP suitable for WiFi pairing of cloudlets.

---
+++ hostapd/start_ap.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Disable Network Manager use of Wifi and create wlan0 interface
+nmcli nm wifi off
+rfkill unblock wlan
+sudo ifconfig wlan0 up 192.168.2.1 netmask 255.255.255.0
+
+# Stop the dnsmasq instance used by libtirtd.
+sudo killall dnsmasq
+sudo stop libvirt-bin
+sudo service bind9 stop
+
+# Start dnsmasq to act as DHCP server
+sudo dnsmasq --dhcp-authoritative --interface=wlan0 --dhcp-range=192.168.2.20,192.168.2.100,255.255.255.0,4h
+
+# Start hostapd
+sudo hostapd -B ./hostapd.conf
---
+++ hostapd/stop_ap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Turn off the hotspot.
+sudo pkill -f 'hostapd -B ./hostapd.conf'
+
+# Undo changes to return control to NM.
+sudo ifconfig wlan0 down
+rfkill block wlan
+nmcli nm wifi on
+
+# Restart libvirtd and its dnsmasq.
+sudo killall dnsmasq
+sudo start libvirt-bin
+sudo service bind9 start
