783719d33b14b4367dfa44d61477b167f5056cd2
Keegan Williams <kmwilliams@sei.cmu.edu>
2016-03-17 13:59:05 -0400

    Missed a few changes to make the ranaming in the previous commit work

--- pycloud/manager/controllers/__init__.py
+++ pycloud/manager/controllers/__init__.py
@@ -51,7 +51,7 @@ def load_controllers():
     from pycloud.manager.controllers.apps import AppsController
     from pycloud.manager.controllers.export import ExportController
     from pycloud.manager.controllers.importsvm import ImportController
-    from pycloud.manager.controllers.pairing import PairingController
+    from pycloud.manager.controllers.devicespairing import DevicesPairingController
     from pycloud.manager.controllers.devices import DevicesController
     from pycloud.manager.controllers.auth import AuthController
 
