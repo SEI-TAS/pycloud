0d17cbd9aadeed643527d1110a1f5f39007f0028
secheverria <sebastian.echeverria@gmail.com>
2016-09-09 19:16:31 -0300

    Cloudlet Pairing: encapsulated ad-hoc code in separate module.

--- pycloud/manager/controllers/cloudletpairing.py
+++ pycloud/manager/controllers/cloudletpairing.py
@@ -46,6 +46,8 @@ from pycloud.pycloud.model.deployment import Deployment
 
 from pycloud.pycloud.utils import ajaxutils
 
+from pycloud.pycloud.network import wifi_adhoc
+
 import subprocess
 
 log = logging.getLogger(__name__)
@@ -86,13 +88,16 @@ class CloudletPairingController(BaseController):
             id = "10.10.10.10"
             port = "1723"
             name = "WiFi1"
-            curr_device = WiFiSKADevice({'host': id, 'port': int(port), 'name': name, 'secret': secret})
+
+            adhoc_mode_enabled = wifi_adhoc.enable_adhoc_mode(id)
 
             # Now the pairing process will be followed, generating all required credentials.
             # The first step is to connect to the device.
-            ap = curr_device.start_ap()
-            if not ap:
-                raise Exception("Could not start AP on local NIC.")
+            #ap = curr_device.start_ap()
+            if not adhoc_mode_enabled:
+                raise Exception("Could not start ad-hoc mode on local NIC.")
+
+            curr_device = WiFiSKADevice({'host': id, 'port': int(port), 'name': name, 'secret': secret})
             curr_device.listen()
 
             #i = 1
@@ -144,14 +149,17 @@ class CloudletPairingController(BaseController):
         page.ssid = host + "-" + temp #ssid should be "<cloudlet machine name>-<alphanumeric and 6 symbols long>"
         psk = ''.join(random.sample(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"], 8))
         page.psk = psk #psk should be alphanumeric and 8 symbols long
-        command = "sed -e \"s/xxxx/" + page.ssid + "/g\" < hostapd/wpa.conf > hostapd/wpa-tmp.conf"
-        print "1 " + command
-        cmd = subprocess.Popen(command, shell=True, stdout=None)
 
-        cmd.wait()
-        command = "sed -e \"s/yyyy/" + page.psk + "/g\" < hostapd/wpa-tmp.conf > hostapd/wpa-nic.conf"
-        print "2 " + command
-        cmd = subprocess.Popen(command, shell=True, stdout=None)
+        #command = "sed -e \"s/xxxx/" + page.ssid + "/g\" < hostapd/wpa.conf > hostapd/wpa-tmp.conf"
+        #print "1 " + command
+        #cmd = subprocess.Popen(command, shell=True, stdout=None)
+
+        #cmd.wait()
+        #command = "sed -e \"s/yyyy/" + page.psk + "/g\" < hostapd/wpa-tmp.conf > hostapd/wpa-nic.conf"
+        #print "2 " + command
+        #cmd = subprocess.Popen(command, shell=True, stdout=None)
+
+        wifi_adhoc.configure_wpa2_params(page.ssid, page.psk)
 
         return page.render()
 
@@ -174,23 +182,30 @@ class CloudletPairingController(BaseController):
             curr_device = None
             if connection == 'wifi':
                 adapter_address = get_adapter_name()
-                command = "sed -e \"s/xxxx/" + ssid + "/g\" < hostapd/wpa.conf > hostapd/wpa-tmp.conf"
-                print "1P " + command
-                cmd = subprocess.Popen(command, shell=True, stdout=None)
-
-                cmd.wait()
-                command = "sed -e \"s/yyyy/" + psk + "/g\" < hostapd/wpa-tmp.conf > hostapd/wpa-nic.conf"
-                print "2P " + command
-                cmd = subprocess.Popen(command, shell=True, stdout=None)
-                #command = "wpa_passphrase " + ssid + " " + psk + ">hostapd/wpa.conf"
+                #command = "sed -e \"s/xxxx/" + ssid + "/g\" < hostapd/wpa.conf > hostapd/wpa-tmp.conf"
+                #print "1P " + command
                 #cmd = subprocess.Popen(command, shell=True, stdout=None)
-                command = "sudo wpa_supplicant -B -Dnl80211,wext -i" + adapter_address + " -chostapd/wpa-nic.conf"
-                cmd = subprocess.Popen(command, shell=True, stdout=None)
-                #port = request.params.get('port', None)
-                #name = request.params.get('name', None)
+
+                #cmd.wait()
+                #command = "sed -e \"s/yyyy/" + psk + "/g\" < hostapd/wpa-tmp.conf > hostapd/wpa-nic.conf"
+                #print "2P " + command
+                #cmd = subprocess.Popen(command, shell=True, stdout=None)
+
                 id = "10.10.10.1"
                 port = "1723"
                 name = "WiFi1"
+
+                wifi_adhoc.configure_wpa2_params(ssid, psk)
+                wifi_adhoc.enable_adhoc_mode(id)
+
+                #command = "wpa_passphrase " + ssid + " " + psk + ">hostapd/wpa.conf"
+                #cmd = subprocess.Popen(command, shell=True, stdout=None)
+                #command = "sudo wpa_supplicant -B -Dnl80211,wext -i" + adapter_address + " -chostapd/wpa-nic.conf"
+                #cmd = subprocess.Popen(command, shell=True, stdout=None)
+
+                #port = request.params.get('port', None)
+                #name = request.params.get('name', None)
+
                 curr_device = WiFiSKADevice({'host': id, 'port': int(port), 'name': name, 'secret': secret})
                 successful_connection = curr_device.connect("10.10.10.10", port, "WiFiAP")
                 if not successful_connection:
