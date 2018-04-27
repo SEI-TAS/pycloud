dc4adc6b6bfc06a6258a60199f7ecef2a9efc3ae
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2018-04-27 16:22:54 -0400

    Undid use of libvirt-python since it has compatibility issues depending on the version of libvirt in the curren distro

--- scripts/install_pip_packages.sh
+++ scripts/install_pip_packages.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 source ../env/bin/activate
 pip install -r ../requirements.txt
+
+# Copy libvirt python packages to the env (there is a high likelyhood of compatibility issues if installed via pip).
+cp /usr/lib/python2.7/dist-packages/*libvirt* ../env/lib/python2.7/site-packages/
