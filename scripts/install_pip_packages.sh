167cf47eedb0f1d9d56ada8aeec9a77cff41cd1f
Sebastian Echeverria <sebastian.echeverria@gmail.com>
2018-04-27 15:41:13 -0400

    Updating installation of libvirt python bindings to be more standard.

--- scripts/install_pip_packages.sh
+++ scripts/install_pip_packages.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 source ../env/bin/activate
 pip install -r ../requirements.txt
-
-# Copy libvirt python packages to the env (there is no way to install them directly through pip).
-cp /usr/lib/python2.7/dist-packages/*libvirt* ../env/lib/python2.7/site-packages/
